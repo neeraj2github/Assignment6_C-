b901e2a68c1885ab0d9bcfe0d7e98d584f823a4c
Neeraj chaubey <20193043neerajchaubey.it@gmail.com>
2023-02-20 18:24:44 +0530

    Pushing Assignment 6

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+public class Program
+{
+    static async Task Main()
+    {
+        Console.Write("Enter URL: ");
+        string url = Console.ReadLine();
+
+        string content = await DownloadContentAsync(url);
+        await WriteContentToFileAsync("A.txt", content);
+
+        Console.WriteLine("Content written to file.");
+    }
+
+    static async Task<string> DownloadContentAsync(string url)
+    {
+        using (var client = new HttpClient())
+        {
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+
+    static async Task WriteContentToFileAsync(string fileName, string content)
+    {
+        using (var writer = new StreamWriter(fileName))
+        {
+            await writer.WriteAsync(content);
+        }
+    }
+}
